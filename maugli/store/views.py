aba59c9c10448debbf764f9d3621b7f6c9af2510
IVaN4B <ivan4b69@gmail.com>
2016-06-05 02:41:21 +0300

    Use BaseView

--- maugli/store/views.py
+++ maugli/store/views.py
@@ -1,16 +1,9 @@
 from django.http import HttpResponse
-from django.views.generic import TemplateView
 from django.conf import settings
 
-class CatalogView(TemplateView):
+from conf.views import BaseView
+
+class CatalogView(BaseView):
 	template_name = "base_catalog.html"
-	def get_context_data(self, **kwargs):
-		context = super(CatalogView, self).get_context_data(**kwargs)
-		context["css_path"] = settings.CSS_PATH
-		context["section"] = {
-				"title": "Каталог — "+settings.SITE_NAME,
-				"style": "",
-		}
-
-		return context
-
+	title = "Каталог"
+	content_title = "Список игрушек"
