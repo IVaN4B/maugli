7e45039b05b9cc3d289b0c919036e163eb68ff11
IVaN4B <ivan4b69@gmail.com>
2016-06-05 02:33:05 +0300

    Added loading time measuring

---
+++ maugli/conf/middlewares.py
@@ -0,0 +1,16 @@
+import time
+
+class StatsMiddleware(object):
+	def process_request(self, request):
+		# "Store the start time when the request comes in."
+		request.start_time = time.time()
+
+	def process_template_response(self, request, response):
+
+		# "Calculate and output the page generation duration"
+		# Get the start time from the request and calculate how long
+		# the response took.
+		duration = time.time() - request.start_time
+
+		response.context_data["load_time"] = duration
+		return response
