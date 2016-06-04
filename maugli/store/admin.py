e8b86cb0259f3715eddf6249b4ab11d825682244
IVaN4B <ivan4b69@gmail.com>
2016-06-05 02:41:53 +0300

    Register models

--- maugli/store/admin.py
+++ maugli/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from store.models import Category, Column, Product
+
+admin.site.register(Category)
+admin.site.register(Column)
+admin.site.register(Product)
 # Register your models here.
