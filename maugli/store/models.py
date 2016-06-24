bbf2964ce4026d56d0a70719475ed608ec6a749e
IVaN4B <ivan4b69@gmail.com>
2016-06-24 23:53:06 +0300

    Added columns

--- maugli/store/models.py
+++ maugli/store/models.py
@@ -1,20 +1,48 @@
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-# Create your models here.
 
 class Category(models.Model):
     class Meta:
         verbose_name_plural = "categories"
-    code         = models.IntegerField()
-    name         = models.CharField(max_length=255, blank=True)
+    code        = models.IntegerField()
+    name        = models.CharField(max_length=255, blank=True)
 
-class Column(models.Model):
-    name        = models.CharField(max_length=255)
-    type        = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    value        = GenericForeignKey('type', 'id')
+class AbstractColumn(models.Model):
+    class Meta:
+        abstract = True
+    STRING  = "string"
+    INT     = "int"
+    DECIMAL = "decimal"
+    TEXT    = "text"
+    TYPE_CHOICES = (
+        (STRING,  "String"),
+        (INT,     "Integer"),
+        (DECIMAL, "Decimal"),
+        (TEXT,    "Text"),
+    )
+    #type        = models.CharField(max_length=255, choices=TYPE_CHOICES,
+    #                                               default=STRING)
+
+class TitleColumn(AbstractColumn):
+    class Meta:
+        verbose_name = 'title'
+    value       = models.CharField(max_length=255, blank=True)
+
+class PriceColumn(AbstractColumn):
+    class Meta:
+        verbose_name = 'price'
+    value       = models.DecimalField(max_digits=20, decimal_places=2)
 
 class Product(models.Model):
     code        = models.IntegerField()
     categories  = models.ManyToManyField(Category)
-    columns        = models.ManyToManyField(Column)
+    title       = models.ForeignKey(TitleColumn, on_delete=models.CASCADE)
+    price       = models.ForeignKey(PriceColumn, on_delete=models.CASCADE)
+    def get_title(self):
+        return self.title.value
+    def get_price(self):
+        return self.price.value
+    def get_image_url(self):
+        domain = "http://maugli-toys.com"
+        return "%s/photos/%s.jpg" % (domain, self.code)
