7a3f9a51ed9b5c4a0795170906ffa88835a64c50
IVaN4B <ivan4b69@gmail.com>
2016-06-25 03:15:32 +0300

    Removed section dependency

--- maugli/menu/templatetags/menu.py
+++ maugli/menu/templatetags/menu.py
@@ -7,9 +7,12 @@ register = template.Library()
 @register.inclusion_tag('snippets/menu.html', takes_context=True)
 def menu(context, name, *args, **kwargs):
     menu = Menu.objects.get(name=name)
+    url = '/'
+    url_match = context['request'].resolver_match.args
+    if url_match:
+        url = "/"+url_match[0]
     links = menu.links.order_by("weight").all()
     title = menu.title
     if "title" in kwargs:
         title = kwargs["title"]
-    return {'title': title, 'menu': menu, 'links': links,
-            'url': context['section']['url'] }
+    return {'title': title, 'menu': menu, 'links': links, 'url': url }
